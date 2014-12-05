1bdf4311f81bfec9584042e5647b685a666ea602
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2014-12-04 22:06:58 -0500

    Fix rxnorm setup call

--- rxnorm/import.sh
+++ rxnorm/import.sh
@@ -4,4 +4,4 @@
 
 git clone http://github.com/chb/py-umls && cd py-umls
 sh databases/rxnorm.sh  ..
-EXPORT_TYPE=csv DID_SOURCE_FOR_SETUP=did python2 rxnorm_link_run.py
+EXPORT_TYPE=sqlite SQLITE_FILE=databases/rxnorm.db DID_SOURCE_FOR_SETUP='did' python3 rxnorm_link_run.py
